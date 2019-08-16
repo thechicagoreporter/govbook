da7b3c341abf0c3c930633e30f09c1602e6910ba
David Eads <davideads@gmail.com>
2019-08-16 00:10:40 -0500

    update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `IL Local Govt Phonebook`,
+    description: `Look up local government contacts in Illinois.`,
+    author: `The Chicago Reporter`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
