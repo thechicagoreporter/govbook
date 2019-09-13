bf7c23400302cef4f74a0f5180cf6a1a77824408
David Eads <davideads@gmail.com>
2019-09-13 14:27:50 -0500

    bump hardcoded updated time

--- src/components/table.js
+++ src/components/table.js
@@ -263,7 +263,7 @@ class Table extends React.Component {
             </div>
             <div className="table-foot-source-line">
               <p>
-                <FormattedMessage id="welcomeMessage.sourceLine" values={{ lastUpdated: "2019-09-12" }} />
+                <FormattedMessage id="welcomeMessage.sourceLine" values={{ lastUpdated: "2019-09-13" }} />
               </p>
             </div>
           </div>
