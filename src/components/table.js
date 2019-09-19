d35cec4999648917090dcb04b92e278e6b3e125c
David Eads <davideads@gmail.com>
2019-09-19 09:21:33 -0500

    2019-09-19 data updates

--- src/components/table.js
+++ src/components/table.js
@@ -263,7 +263,7 @@ class Table extends React.Component {
             </div>
             <div className="table-foot-source-line">
               <p>
-                <FormattedMessage id="welcomeMessage.sourceLine" values={{ lastUpdated: "2019-09-18" }} />
+                <FormattedMessage id="welcomeMessage.sourceLine" values={{ lastUpdated: "2019-09-19" }} />
               </p>
             </div>
           </div>
